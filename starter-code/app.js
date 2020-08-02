fb76e099de4fa2194dbb0f53d14be9c5a0f21d85
kgonzalez1219 <kat@MSI.localdomain>
2020-08-01 20:00:46 -0700

    problem3

--- starter-code/app.js
+++ starter-code/app.js
@@ -30,11 +30,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var product = a * b;
+  var words = `The product of ${a} and ${b} is ${product}.`;
+  return[product, words]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,12 +52,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+var sum = sum(a, b, c );
+var product = multiply(a, b, c);
+
+var stringOne = ('a'+ 'and' + 'b'+'c'+'sum to''sum'.);
+var stringTwo = ('The product of' + 'a' + 'and' + 'b'+ 'and'+ 'c' + 'is' + 'product.');
+return[sum, product];
+
+  
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ //testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
